556bd2e74f509badcfce1160d54f679cb124491a
patrickliu022 <patrickliu022@gmail.com>
2021-12-01 01:25:08 -0800

    added professional stored procedure

---
+++ stored-procedures/insert-professional.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE INSERT_PROFESSIONAL
+    @prof_fname VARCHAR(30),
+    @prof_lname VARCHAR(30),
+    @prof_dob DATE,
+    @gender BIT
+BEGIN TRANSACTION T1
+    INSERT INTO PROFESSIONAL
+    VALUES
+    (@prof_fname, @prof_lname, @prof_dob, @gender)
+COMMIT TRANSACTION T1
+
+-- EXECUTE INSERT_PROFESSIONAL
+-- @prof_fname = '',
+-- @prof_lname = '',
+-- @prof_dob = '',
+-- @gender = ''
+-- GO;
