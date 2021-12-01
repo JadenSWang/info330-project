2dc51acad53fd1dcf6da5e2cc8a17d685d2e9aff
realbp <72233714+realbp@users.noreply.github.com>
2021-12-01 00:17:20 -0800

    Create insert-allowed_countries.sql

---
+++ stored-procedures/insert-allowed_countries.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE INSERT_ALLOWED_COUNTRIES (
+    @prodName VARCHAR(256),
+    @country VARCHAR(50)
+) AS DECLARE @prod_id, country_id INTEGER
+
+BEGIN TRANSACTION T1
+
+SET @prod_id = (SELECT production_id, FROM PRODUCTION, WHERE production_name = @prodName)
+SET @country = (SELECT country_id, FROM COUNTRY, WHERE country_name = @country)
+
+INSERT INTO
+ALLOWED_COUNTRIES(production_id, country_id)
+VALUES (@prod_id, @country)
+COMMIT TRANSACTION T1
