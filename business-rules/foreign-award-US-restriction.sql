11525a40e48e300839e4b6ce7a618cdd245fc775
patrickliu022 <patrickliu022@gmail.com>
2021-12-02 03:27:33 -0800

    edited business rules

--- business-rules/foreign-award-US-restriction.sql
+++ business-rules/foreign-award-US-restriction.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION ForeignAwardUSRestriction()
+CREATE FUNCTION fnForeignAwardUSRestriction()
 RETURNS INTEGER
 AS
 BEGIN
@@ -6,12 +6,12 @@ BEGIN
 DECLARE @RET INTEGER = 0
 IF EXISTS (
     SELECT *
-    FROM NOMINATION N
-        JOIN AWARD A on N.award_id = A.award_id
-        JOIN PRODUCTION P on A.award_id = P.award_id
-        JOIN COMPANY_PROD CP on P.production_id = CP.production_id
-        JOIN COMPANY C on CP.company_id = C.company_id
-        JOIN COUNTRY CO on C.country_id = CO.country_id
+    FROM COMPANY_PROD CP
+        JOIN COMPANY C ON C.company_id = CP.company_id
+        JOIN COUNTRY CO ON CO.country_id = C.country_id
+        JOIN PRODUCTION P ON P.production_id = CP.production_id
+        JOIN PRODUCTION_NOMINATION PN ON PN.production_id = P.production_id
+        JOIN AWARD A ON A.award_id = PN.award_id
     WHERE A.award_name = 'Best Foreign Film'
     AND CO.country_name = 'United States'
 )
@@ -21,6 +21,6 @@ RETURN @RET
 END
 GO
 
-ALTER TABLE NOMINATION
-ADD CONSTRAINT noUSForeignAward
-CHECK (dbo.ForeignAwardUSRestriction() = 0)
+ALTER TABLE PRODUCTION_NOMINATION
+ADD CONSTRAINT noUSForeignAwards
+CHECK (dbo.fnForeignAwardUSRestriction() = 0)
