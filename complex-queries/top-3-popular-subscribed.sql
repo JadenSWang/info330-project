15c334d67f41e3d19086d3e85f9d5d8606599c9a
Jaden Wang <jadenswang@Jadens-MacBook-Pro.local>
2021-12-02 00:56:19 -0800

    popular_subscribed

--- complex-queries/top-3-popular-subscribed.sql
+++ complex-queries/top-3-popular-subscribed.sql
@@ -1,1 +1,10 @@
-CREATE FUNCTION 
+SELECT
+    COMPANY.company_name, count(SUBSCRIPTION.subscription_id) AS num_subscribers
+FROM
+    COMPANY
+    JOIN COMPANY_PROD ON COMPANY_PROD.company_id = COMPANY.company_id
+    JOIN COMPANY_ROLE ON COMPANY_ROLE.company_role_id = COMPANY_PROD.company_role_id
+    JOIN SUBSCRIPTION ON SUBSCRIPTION.company_prod_id = COMPANY_PROD.company_prod_id
+WHERE COMPANY_ROLE.role_name = 'Streaming'
+GROUP BY COMPANY.company_name
+ORDER BY num_subscribers
