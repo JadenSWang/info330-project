609f0e3c51e897d59109978e34550e8336eabd04
patrickliu022 <patrickliu022@gmail.com>
2021-12-10 20:53:27 -0800

    edited rRated movie complex query

--- complex-queries/rRated-movie-nominated-best-cinemotography.sql
+++ complex-queries/rRated-movie-nominated-best-cinemotography.sql
@@ -1,10 +1,14 @@
--- [#9] production type 'Movie' with a rating of 'R-rated' that had been
--- nominated for 'Best Cinemotography'
-SELECT *
+SELECT PRODUCTION.production_name
 FROM PRODUCTION
          JOIN RATING ON PRODUCTION.rating_id = RATING.rating_id
          JOIN PRODUCTION_NOMINATION ON PRODUCTION_NOMINATION.production_id = PRODUCTION.production_id
          JOIN AWARD ON PRODUCTION_NOMINATION.award_id = AWARD.award_id
+         JOIN (
+            SELECT P.production_id
+            FROM PRODUCTION P
+                JOIN STREAM S ON S.production_id = P.production_id
+            GROUP BY P.production_id
+            HAVING COUNT(S.production_id) >= 1
+         ) PC ON PC.production_id = PRODUCTION.production_id
 WHERE AWARD.award_name = 'Best Picture'
     AND RATING.rating_name = 'R'
-
