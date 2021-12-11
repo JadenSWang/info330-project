74a4a7a089d97db4002693c1f62536c4e8246e8c
realbp <72233714+realbp@users.noreply.github.com>
2021-12-10 20:32:05 -0800

    edited complex query

--- complex-queries/most-streamed-action-michael-bay.sql
+++ complex-queries/most-streamed-action-michael-bay.sql
@@ -1,15 +1,28 @@
+-- Brian Park
 -- [#3] production that has been streamed the most in the genre 'Action' that has been directed
--- by professional 'Michael Bae'
-SELECT P.production_name,
-FROM (
-     SELECT P.production_name, COUNT(S.production_id) AS num_stream
-     FROM STREAM S
-        JOIN PRODUCTION P on S.production_id = P.production_id
-        JOIN GENRE G on P.genre_id = G.genre_id
-         )
-WHERE (
-    SELECT professional_fname, professional_lname
-    FROM PROFESSIONAL
-    WHERE professional_fname = 'Michael'
-    AND professional_lname = 'Bay')
-AND G.genre_name = 'Action'
+-- by professional 'Michael Bay' that has also been produced by the company 'Marvel' and streamed
+-- by Customer 'Brian'
+
+SELECT A.production_id, A.production_name
+FROM
+(SELECT P.production_id, P.production_name
+FROM PRODUCTION P
+    JOIN CREDIT C on P.production_id = C.production_id
+    JOIN PROFESSIONAL PR on C.professional_id = PR.professional_id
+    JOIN GENRE G on P.genre_id = G.genre_id
+WHERE G.genre_name = 'Action'
+AND PR.professional_fname = 'Michael'
+AND PR.professional_lname = 'Bay'
+GROUP BY P.production_id, P.production_name) AS A,
+
+(SELECT P.production_id, P.production_name
+FROM CUSTOMER CU
+    JOIN STREAM S on CU.customer_id = S.customer_id
+    JOIN PRODUCTION P on S.production_id = P.production_id
+    JOIN COMPANY_PROD CP on P.production_id = CP.production_id
+    JOIN COMPANY COM on CP.company_id = COM.company_id
+WHERE COM.company_name = 'Marvel Cinematic Universe'
+AND CU.customer_username = 'Brian'
+GROUP BY P.production_id, P.production_name) AS B
+
+WHERE A.production_id = B.production_id
