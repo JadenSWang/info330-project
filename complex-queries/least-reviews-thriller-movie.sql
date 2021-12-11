3965e6e928e7af4e4f06977c639df241f778f291
patrickliu022 <patrickliu022@gmail.com>
2021-12-10 21:48:57 -0800

    least reviewed complex query edited

--- complex-queries/least-reviews-thriller-movie.sql
+++ complex-queries/least-reviews-thriller-movie.sql
@@ -1,12 +1,17 @@
 -- Selina Dinh
 -- [#8] least reviews for production type ‘Movie’ of genre ‘Thriller’
 
-SELECT COUNT(R.review_id) AS num_review
+SELECT P.production_name, COUNT(R.review_id) AS num_review
 FROM REVIEW R
     JOIN PRODUCTION_REVIEW PR on R.review_id = PR.review_id
     JOIN PRODUCTION P on PR.production_id = P.production_id
     JOIN PRODUCTION_TYPE PT on P.production_type_id = PT.production_type_id
     JOIN GENRE ON GENRE.genre_id = P.genre_id
-WHERE GENRE.genre_name = 'Sci-Fi'
-    AND PT.production_name = 'Movie'
+    JOIN (
+        SELECT G.genre_id
+        FROM GENRE G
+        WHERE G.genre_name = 'Sci-Fi'
+    ) GSF ON GSF.genre_id = GENRE.genre_id
+WHERE PT.production_name = 'Movie'
+GROUP BY P.production_name
 ORDER BY num_review DESC
