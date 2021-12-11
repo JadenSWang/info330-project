20d2ea10123d42c226624a365de9d28b613c1395
patrickliu022 <patrickliu022@gmail.com>
2021-12-10 21:33:57 -0800

    added longest subscribed production

---
+++ complex-queries/longest-subscribed-production.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT P.production_name
+FROM SUBSCRIPTION S
+    JOIN COMPANY_PROD CP ON CP.company_prod_id = S.company_prod_id
+    JOIN PRODUCTION P ON P.production_id = CP.production_id
+WHERE S.duration_days >= ALL (
+    SELECT S1.duration_days
+    FROM SUBSCRIPTION S1
+    WHERE S1.duration_days > 5
+)
